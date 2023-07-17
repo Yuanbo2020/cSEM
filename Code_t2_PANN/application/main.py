a2a67d44e71037fc115c8c872a2ef7b48ae75463
Yuanbo2020 <73268506+Yuanbo2020@users.noreply.github.com>
2023-07-17 19:49:32 +0200

    Add files via upload

---
+++ Code_t2_PANN/application/main.py
@@ -0,0 +1,82 @@
+import sys, os, argparse
+
+sys.path.append(os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+from framework.utilities import *
+from framework.models_pytorch import *
+from framework.data_generator import *
+from framework.processing import *
+
+
+class Logger(object):
+    def __init__(self, filename='default.log', stream=sys.stdout):
+        self.terminal = stream
+        self.log = open(filename, 'w')
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
+
+def main(argv):
+    label_type = ''
+
+    batch_size = 64
+    epochs = 100
+
+    basic_name = 'system'
+
+    suffix, system_name = define_system_name(basic_name=basic_name)
+    system_path = os.path.join(os.getcwd(), system_name)
+
+    Model = PANN
+
+    models_dir = system_path + label_type
+
+    classes_num = len(config.labels)
+    model = Model(classes_num, batchnormal=True)
+
+    print(model)
+
+    if config.cuda:
+        model.cuda()
+
+    # Data generator
+    hdf5_file = os.path.join(config.dataset_dir, 'PANN', 'kong_mel', 'development.h5')
+    print('using: ', hdf5_file)
+    generator = DataGenerator_scene_event_D2018(hdf5_path=hdf5_file,
+                                          normalization=True,
+                                          dev_train_csv=config.dev_train_csv,
+                                          dev_validate_csv=config.dev_validate_csv)
+
+    training = training_process
+    training(generator, model, config.cuda, models_dir, label_type=label_type)
+    print('Training is done!!!')
+
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main(sys.argv))
+    except (ValueError, IOError) as e:
+        sys.exit(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
