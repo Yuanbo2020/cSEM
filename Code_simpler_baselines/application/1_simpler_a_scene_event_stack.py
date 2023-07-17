90468eb3fd0afb5c1e17b76fac0c232e57079f9d
Yuanbo2020 <73268506+Yuanbo2020@users.noreply.github.com>
2023-07-17 17:57:18 +0200

    Add files via upload

---
+++ Code_simpler_baselines/application/1_simpler_a_scene_event_stack.py
@@ -0,0 +1,69 @@
+import sys, os, argparse
+
+sys.path.append(os.path.split(os.path.dirname(os.path.realpath(__file__)))[0])
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+from framework.utilities import *
+from framework.models_pytorch import *
+from framework.data_generator import *
+from framework.processing import *
+
+
+
+
+def main(argv):
+    label_type = ''
+    cuda = config.cuda
+    batch_size = 64
+    epochs = 100
+
+    basic_name = 'system'
+
+    suffix, system_name = define_system_name(basic_name=basic_name)
+    system_path = os.path.join(os.getcwd(), system_name)
+
+    Model = Simpler_baseline_scene_event_stack
+
+    models_dir = system_path + label_type
+
+    classes_num = len(config.labels)
+    event_class = 527
+    model = Model(classes_num, event_class, batchnormal=True)
+
+    print(model)
+
+    if config.cuda:
+        model.cuda()
+
+    # Data generator
+    hdf5_file = os.path.join(config.dataset_dir, 'PANN', 'kong_mel', 'development.h5')
+    print('using: ', hdf5_file)
+    generator = DataGenerator_scene_event(hdf5_path=hdf5_file, normalization=True)
+
+    training = training_process
+    training(generator, model, cuda, models_dir, epochs, batch_size,  lr_init=config.lr_init)
+    print('Training is done!!!')
+
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main(sys.argv))
+    except (ValueError, IOError) as e:
+        sys.exit(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
